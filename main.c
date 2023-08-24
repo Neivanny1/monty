b45b7b539eb96bb2c9d1d8b0c37d0ce1937d4bcb
Neivanny1 <nevileneivanny@gmail.com>
2023-08-24 16:22:33 +0300

    All mandatory tasks done

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-#include"lists.h"
+#include"monty.h"
 /**
  * main - Main entry
  * Description: Monty bytecodes files interpreter
@@ -9,13 +9,7 @@
 int main(int argc, char **argv)
 {
 	if (argc != 2)
-    {
 		error_arguments();
-        char *filename = argv[1];
-
-        fprintf(stderr,"Error: Can't open %s\n", filename);
-        exit(EXIT_FAILURE);
-    }
 	open_and_read(argv);
 	return (0);
 }
